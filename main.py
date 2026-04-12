998e4960ca7a54fee03fe7911d05d0612a0bd7ad
playermix-source <shamakhan89453@gmail.com>
2026-04-12 14:28:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,6 @@ from config import API_ID, API_HASH, BOT_TOKEN
 import database as db
 import apis
 
-
-
-
-
-
-
-
 app = Client("beatnova_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 BOT_NAME = "BeatNova"
